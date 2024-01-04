124c13469cce1dfd1826ba039110dee11fc421db
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2024-01-03 20:23:47 -0700

    finished up to chapter 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,12 +20,7 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -36,17 +31,13 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
-  try {
-    console.log('Fetching latest invoice data...');
-    await new Promise((resolve) => setTimeout(resolve, 2000));  
+  try { 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
-    console.log('Invoice data fetch completed after 2 seconds.');
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
@@ -70,14 +61,11 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-    console.log('Fetching card data...');
-    await new Promise((resolve) => setTimeout(resolve, 1000));  
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
     ]);
-    console.log('Invoice data fetch completed after 1 second.');
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
